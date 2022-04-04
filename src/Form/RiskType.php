b20f868032664842d15acfdbb58af2c2cfdcb514
Mehdi <mhdlaribi60@gmail.com>
2022-04-04 16:15:52 +0200

    feat: modification Risk

--- src/Form/RiskType.php
+++ src/Form/RiskType.php
@@ -2,7 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Probability;
 use App\Entity\Risk;
+use App\Entity\Severity;
+use App\Repository\ProbabilityRepository;
+use App\Repository\SeverityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,9 +20,25 @@ class RiskType extends AbstractType
             ->add('name')
             ->add('identification_date')
             ->add('resolved_date')
-            ->add('severity')
-            ->add('propability')
-            ->add('project')
+            ->add('severity',EntityType::class,[
+                'label' => 'Severity',
+                'choice_label'=>'name',
+                'class'=> Severity::class,
+                'query_builder' => function(SeverityRepository $er){
+                    return $er->createQueryBuilder('sv')
+                    ->orderBy('sv.id');
+                },
+            ])
+            ->add('probability', EntityType::class,[
+                'label'=> 'Probability',
+                'class'=> Probability::class,
+                'choice_label'=>'name',
+                'query_builder' => function(ProbabilityRepository $er){
+                    return $er->createQueryBuilder('po')
+                    ->orderBy('po.id');
+                },
+            ])
+            
         ;
     }
 
